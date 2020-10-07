3c9dfbd01923480651ac24421bd3a05994af375b
AdamOB1290 <codingjourney@outlook.com>
2020-10-07 17:52:06 +0100

    Components Created (Header, Home, Product, Checkout ) // React Router //  React Context API ( Add to/remove from Basket )

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAJjRWPrIFTfFCu8C84lkN-JhQZYSIBMM0",
+    authDomain: "clone-e5f47.firebaseapp.com",
+    databaseURL: "https://clone-e5f47.firebaseio.com",
+    projectId: "clone-e5f47",
+    storageBucket: "clone-e5f47.appspot.com",
+    messagingSenderId: "819933079636",
+    appId: "1:819933079636:web:ebd60d1f03908a0e05a5c1",
+    measurementId: "G-315KXJPR17"
+  };
